73611367f52debc8c7bae1bf5187d7f69980f0e9
SL33PZ <93669183+SL33PZ@users.noreply.github.com>
2023-06-30 18:02:15 +0200

    Update bashSetup.sh

--- bashSetup.sh
+++ bashSetup.sh
@@ -7,6 +7,6 @@ wget https://github.com/SL33PZ/cfgs/settings/zshrc -O "$HOME/.zshrc"
 sh -c "$(wget https://github.com/SL33PZ/yay.bsx -O -)";
 
 wget https://github.com/SL33PZ/cfgs/pkglist.txt -O "$HOME/Downloads/pkglist.txt"
-cd "$HOME/Downloads" && yay -S --needed --noconfirm --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --noredownload --skippgpcheck --nouseask
+cd "$HOME/Downloads" && yay -S --needed --noconfirm --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --noredownload --skippgpcheck --nouseask - < pkglist.txt
 
 mv "$HOME/.bashrc.backup" "$HOME/.bashrc"
