07e308777ba0799678eb7b68afe7afa2a87da521
Leonidas <leonidas@Ubuntu.Temp>
2023-07-02 10:55:47 +0200

    Updte

--- bashSetup.sh
+++ bashSetup.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-mv "$HOME/settings/*" "$HOME/.config"
 
-wget https://github.com/SL33PZ/cfgs/settings/zshrc -O "$HOME/.zshrc"
-
-sh -c "$(wget https://github.com/SL33PZ/yay.bsx -O -)";
-
-wget https://github.com/SL33PZ/cfgs/pkglist.txt -O "$HOME/Downloads/pkglist.txt"
-cd "$HOME/Downloads" && yay -S --needed --noconfirm --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --noredownload --skippgpcheck --nouseask - < pkglist.txt
-
-mv "$HOME/.bashrc.backup" "$HOME/.bashrc"
